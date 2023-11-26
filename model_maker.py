898f7dd08fa276fa005f19a30b2873d0fb6c9e56
KamranM5 <81294709+KamranM5@users.noreply.github.com>
2023-11-26 10:51:04 +0300

    Update and rename main.py to model_maker.py

--- model_maker.py
+++ model_maker.py
@@ -4,7 +4,7 @@ from keras.models import Sequential, load_model
 from keras.layers import Dense
 
 # Чтение данных из csv файла
-data = pd.read_csv("full.csv", delimiter="|")
+data = pd.read_csv("training.csv", delimiter="|")
 X = np.array(data.iloc[:, :-1])  # Входные параметры (первые 14 столбцов)
 y = np.array(data.iloc[:, -1])  # Выходной параметр (последний столбец)
 
@@ -17,20 +17,7 @@ model.add(Dense(1))  # Выходной слой с одним нейроном 
 model.compile(loss='mse', optimizer='adam')  # Используем среднеквадратичную ошибку (MSE) в качестве функции потерь и алгоритм оптимизации Adam
 
 # Обучение модели
-model.fit(X, y, epochs=850, batch_size=32)  # Обучение модели на входных и выходных данных для 850 эпох с размером пакета 32
+model.fit(X, y, epochs=2, batch_size=32)  # Обучение модели на входных и выходных данных для 2 эпох с размером пакета 32
 
 # Сохранение модели
 model.save("model.h5")
-
-# Загрузка модели
-model = Sequential()
-model = load_model("model.h5") 
-
-# Рассчет для новых данных
-new_data = pd.read_csv("for_test.csv", delimiter="|")
-X_new = np.array(new_data.iloc[:, :])  # Входные параметры новых данных
-predictions = model.predict(X_new)  # Предсказание для новых данных
-
-# Сохранение результатов в файл
-results = pd.DataFrame(predictions, columns=["Параметр_15"])  # Создаем DataFrame с предсказаниями
-results.to_csv("test_results.csv", index=False, sep="|")  # Сохраняем результаты в csv файл
